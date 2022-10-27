1aefa0ed970c3b97e4947e166d5a143e749da8d4
kumailDoc <dkumail.my.centennialcollege.ca>
2022-10-27 15:25:16 -0400

    buttons and routers added

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //local MongoDB deployment ->
+  //URI: "mongodb://127.0.0.1/emp",
+  URI: "mongodb+srv://KumailDoc:LlXfUaUPul9Y0IYM@cluster0.yjdcwto.mongodb.net/emp?retryWrites=true&w=majority",
+};
